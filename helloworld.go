b802bf48ac850e6bf5b12aec18115e0b66601735
arnaizaitor <arnaizaitor@gmail.com>
2024-08-31 23:00:45 +0100

    wip

--- helloworld.go
+++ helloworld.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello, Goldfinger!")
+	fmt.Println("")
+
+	var l int = len(os.Args) - 1
+	var s string
+
+	for i := 1; i < len(os.Args); i++ {
+		s += os.Args[i] + " "
+	}
+
+	fmt.Printf("The %v input arguments are: %v", l, s)
 }
